279d33afaf213b342e1842a5ffffa1e7d7478f4c
sathwik413 <sathwikreddypathuri0413@gmail.comgit config --global user.name sathwik413git config user.email sathwikreddypathuri0413@gmail.com>
2026-09-19 21:07:21 -0400

    practice: while & for loops - break vs continue

---
+++ basics/loops/break_vs_continue.py
@@ -0,0 +1,16 @@
+# continue
+i = 0
+while i < 5:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+
+# break
+i = 0
+while i < 5:
+    i += 1
+    if i == 3:
+        break
+    print(i)
