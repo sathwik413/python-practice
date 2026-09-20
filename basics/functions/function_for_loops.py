6d1b2670a59b072543d0dea7fed5206dc36aeab0
sathwik413 <sathwikreddypathuri0413@gmail.comgit config --global user.name sathwik413git config user.email sathwikreddypathuri0413@gmail.com>
2026-09-19 21:25:30 -0400

    practice: loops - input validation using functions

---
+++ basics/functions/function_for_loops.py
@@ -0,0 +1,17 @@
+def main():
+    meow(get_number())
+
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
